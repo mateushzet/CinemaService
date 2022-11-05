1465dc072c9191ddd862a14d9fa6393d8d98da52
mateushzet <mateushzett@gmail.com>
2022-11-05 09:44:52 +0100

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Klient klient = new Klient();
-        Seans seans = new Seans("tytul",10,);
+        Klient klient = new Klient("ziecina","mateusz","mati@onet.pl","minionki",293849489,);
+        Seans seans = new Seans("tytul",10, 10, 18);
     }
 }
--- src/Seans.java
+++ src/Seans.java
@@ -6,7 +6,7 @@ public class Seans implements Serializable {
     int dzien;
     int godzina;
     int ograniczeniaWiekowe;
-    HashMap<Character, HashMap<Integer, Boolean>> liczbaMiejsc ;
+    HashMap<Character, HashMap<Integer, Integer>> liczbaMiejsc ;
 
     @Override
     public String toString() {
@@ -24,10 +24,21 @@ public class Seans implements Serializable {
         this.dzien = dzien;
         this.godzina = godzina;
         this.ograniczeniaWiekowe = ograniczeniaWiekowe;
-        //this.liczbaMiejsc = liczbaMiejsc;
+        fillMap();
     }
 
     public void fillMap(){
-        liczbaMiejsc.put("a",new HashMap<Integer,Boolean>(1,Boolean.TRUE));
+        for(int i = 0; i<=20; i++){
+            liczbaMiejsc.put('a', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('b', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('c', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('d', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('e', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('f', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('g', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('h', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('i', new HashMap<Integer,Integer>(i,0));
+            liczbaMiejsc.put('j', new HashMap<Integer,Integer>(i,0));
+        }
     }
 }
