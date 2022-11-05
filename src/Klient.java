cd31d69157a6b9f62f65615ae8e65728d7d27753
kpalus <k.palus20@gmail.com>
2022-11-05 09:11:42 +0100

    Klient update

--- src/Klient.java
+++ src/Klient.java
@@ -1,2 +1,8 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Klient {
+    String nazwisko,imie, mail, seans;
+    int telefon;
+    List<String> miejsce = new LinkedList<String>();
 }
