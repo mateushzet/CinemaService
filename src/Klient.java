d8baab6377d2893e1528b6f721063e2f69c9d6a0
kpalus <k.palus20@gmail.com>
2022-11-05 09:25:45 +0100

    Klient update 2

--- src/Klient.java
+++ src/Klient.java
@@ -1,8 +1,32 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.io.Serializable;
 
-public class Klient {
+public class Klient implements  Serializable{
     String nazwisko,imie, mail, seans;
     int telefon;
     List<String> miejsce = new LinkedList<String>();
+
+    public Klient(String nazwisko, String imie, String mail, String seans, int telefon, List<String> miejsce) {
+        this.nazwisko = nazwisko;
+        this.imie = imie;
+        this.mail = mail;
+        this.seans = seans;
+        this.telefon = telefon;
+        this.miejsce = miejsce;
+    }
+
+    @Override
+    public String toString() {
+        return "Klient{" +
+                "nazwisko='" + nazwisko + '\'' +
+                ", imie='" + imie + '\'' +
+                ", mail='" + mail + '\'' +
+                ", seans='" + seans + '\'' +
+                ", telefon=" + telefon +
+                ", miejsce=" + miejsce +
+                '}';
+    }
 }
+
+
