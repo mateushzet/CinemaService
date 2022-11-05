b99a39479a8ddfae9d7a6f8cbd7ea4b531360938
kpalus <k.palus20@gmail.com>
2022-11-05 09:50:04 +0100

    main update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class Main {
     public static void main(String[] args) {
         Klient klient = new Klient("ziecina","mateusz","mati@onet.pl","minionki",293849489);
         Seans seans = new Seans("tytul",10, 10, 18);
+
+        // tworzymy obiekt klasy ObjectOutputStream do zapisywania do pliku
+        ObjectOutputStream wy = new ObjectOutputStream(new FileOutputStream(".\\klient.dat"));
     }
 }
