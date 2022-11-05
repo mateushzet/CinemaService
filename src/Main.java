2accadb3870e4a525a5cee3aff1cb2fb79d0eace
kpalus <k.palus20@gmail.com>
2022-11-05 10:21:21 +0100

    main update final

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,34 @@ import java.io.ObjectOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        Klient mateusz = new Klient("ziecina","mateusz","mati@onet.pl","minionki",293849489);
-        Seans seans = new Seans("tytul",10, 10, 18);
-try {
+        Klient mateusz = new Klient("ziecina", "mateusz", "mati@onet.pl", "minionki", 293849489);
+        Seans seans = new Seans("tytul", 10, 10, 18);
+        try {
 
-    // tworzymy obiekt klasy ObjectOutputStream do zapisywania do pliku
-    ObjectOutputStream wy = new ObjectOutputStream(new FileOutputStream(".\\klient.dat"));
+            // tworzymy obiekt klasy ObjectOutputStream do zapisywania do pliku
+            ObjectOutputStream wy = new ObjectOutputStream(new FileOutputStream(".\\klient.dat"));
 
-    // no i zapisujemy
-    wy.writeObject(mateusz);
-    wy.close();
-}catch (Exception e) {System.out.println("Error");}
+            // no i zapisujemy
+            wy.writeObject(mateusz);
+            wy.close();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
 
+        try {
 // tworzymy obiekt klasy ObjectInputStream do odczytywania z pliku
-        ObjectInputStream we = new ObjectInputStream(new FileInputStream(".\\klient.dat"));
+            ObjectInputStream we = new ObjectInputStream(new FileInputStream(".\\klient.dat"));
 
-        // odczytujemy z pliku; (klient) - to rzutowanie z Object na klient
-        Klient k1 = (Klient) we.readObject();
+            // odczytujemy z pliku; (klient) - to rzutowanie z Object na klient
+            Klient k1 = (Klient) we.readObject();
 
-        // i ładnie na konsolę wyrzucamy wynik
-        System.out.println(k1);
-        Klient p2 = (Klient)we.readObject();
-        System.out.println(p2);
-        we.close();
+            // i ładnie na konsolę wyrzucamy wynik
+            System.out.println(k1);
+            Klient p2 = (Klient) we.readObject();
+            System.out.println(p2);
+            we.close();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
 }
