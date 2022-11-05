68566081568f902a875b2438761d96d087d021e7
mateushzet <mateushzett@gmail.com>
2022-11-05 08:52:15 +0100

    pierwszy update

---
+++ src/Seans.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Seans {
+    String tytul;
+    int dzien;
+    int godzina;
+    int ograniczeniaWiekowe;
+    HashMap<Character, HashMap<Integer, Boolean>> liczbaMiejsc ;
+}
