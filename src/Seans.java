bcd0c97ee27a1343e58e9bb4cfa811827bd8103b
mateushzet <mateushzett@gmail.com>
2022-11-05 08:57:08 +0100

    update pracownik

--- src/Seans.java
+++ src/Seans.java
@@ -1,9 +1,19 @@
+import java.io.Serializable;
 import java.util.HashMap;
 
-public class Seans {
+public class Seans implements Serializable {
     String tytul;
     int dzien;
     int godzina;
     int ograniczeniaWiekowe;
+
+    public Seans(String tytul, int dzien, int godzina, int ograniczeniaWiekowe, HashMap<Character, HashMap<Integer, Boolean>> liczbaMiejsc) {
+        this.tytul = tytul;
+        this.dzien = dzien;
+        this.godzina = godzina;
+        this.ograniczeniaWiekowe = ograniczeniaWiekowe;
+        this.liczbaMiejsc = liczbaMiejsc;
+    }
+
     HashMap<Character, HashMap<Integer, Boolean>> liczbaMiejsc ;
 }
