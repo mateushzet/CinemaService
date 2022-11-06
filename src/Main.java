fa6aa1a93c904e9b2929dc83a5caf3cb7b011468
mateushzet <mateushzett@gmail.com>
2022-11-06 09:27:48 +0100

    main

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,12 @@ import java.io.ObjectOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        Klient mateusz ;
-        Seans minionki ;
-        Seans auta ;
-        Seans pila ;
-        try {
+        Klient klient1 = null ;
+        Seans minionki = null ;
+        Seans auta = null ;
+        Seans pila = null ;
 
-//            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(".\\klient.dat"));
-//
-//          out.writeObject(auta);
-//          out.writeObject(minionki);
-//          out.writeObject(pila);
-//          out.writeObject(mateusz);
-//
-//            out.close();
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
+        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ odczyt
 
         try {
 
@@ -39,14 +28,32 @@ public class Main {
             System.out.println(minionki.toString());
             System.out.println(pila.toString());
 
-            Klient Mateusz = (Klient) in.readObject();
-            System.out.println(Mateusz.toString());
+            klient1 = (Klient) in.readObject();
+            System.out.println(klient1.toString());
 
             in.close();
         } catch (Exception e) {
             System.out.println("Error");
         }
+
         TextInterface textInterface = new TextInterface();
         textInterface.buyTicket();
+
+        //@@@@@@@@@@@@@@@@@@@@@@@@@@@@ zapis
+
+        try {
+
+          ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(".\\klient.dat"));
+          out.writeObject(textInterface.klient);
+          out.writeObject(auta);
+          out.writeObject(minionki);
+          out.writeObject(pila);
+          out.writeObject(klient1);
+
+
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
     }
 }
